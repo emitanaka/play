ddcee776f502c7557d527d2342564607f8299881
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-08-04 15:52:53 +1000

    Initial commit

---
+++ R/my-first-function.R
@@ -0,0 +1,13 @@
+
+
+#' Simple addition
+#'
+#' @param x A number
+#' @param y Another number
+#' @examples
+#' add(3, 4)
+#'
+#' @export
+add <- function(x, y) {
+  x + y
+}
---
+++ data-raw/mydata.R
@@ -0,0 +1,5 @@
+## code to prepare `mydata` dataset goes here
+
+mydata <- list(x = 3, y = 4)
+
+usethis::use_data(mydata, overwrite = TRUE)
